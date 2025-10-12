4b7d509ab9f547ddbc1e11bc8c90315331f6c367
huangen <994554503@qq.com>
2025-10-12 21:40:35 +0800

    QrToInvoice Init

---
+++ src/test/java/com/he/qrtoinvoice/QrToInvoiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.he.qrtoinvoice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QrToInvoiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
